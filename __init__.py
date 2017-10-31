5a9f87c45d93fe5b8aee077fe7f2507067071df1
bearbox92 <31713772+bearbox92@users.noreply.github.com>
2017-10-31 14:38:56 -0700

    website is working?!

--- __init__.py
+++ __init__.py
@@ -1,23 +1,12 @@
 from flask import Flask 
 from flaskrouter import flaskrouter
 
-import models
+import models as models
 
-def create_app():
-	app = Flask(__name__)
-        SQLALCHEMY_DATABASE_URI = \
-        '{engine}://{username}:{password}@{hostname}/{database}'.format(
-            engine='postgres',
-            username='postgres',
-            password='groupPassword',
-            hostname='35.193.209.24:5432',
-database='postgres')
-        app.config['SQLALCHEMY_DATABASE_URI'] = SQLALCHEMY_DATABASE_URI
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-        models.db.init_app(app)
-        app.register_blueprint(flaskrouter)
-	return app
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:groupPassword@/postgres?host=/cloudsql/overwatchglam-181020:us-central1:overwatch2'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+models.db.init_app(app)
+app.register_blueprint(flaskrouter)
 
-if __name__ == "__main__":
-    app = create_app()
-    app.run()
+
