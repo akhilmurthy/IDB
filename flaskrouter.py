5009ffa8413037f87264032cd3d7aed8755820a8
bearbox92 <31713772+bearbox92@users.noreply.github.com>
2017-11-05 13:32:02 -0800

    Pagination is done

--- flaskrouter.py
+++ flaskrouter.py
@@ -22,24 +22,6 @@ def about():
     """
     return render_template('about.html')
 
-
-
-@flaskrouter.route('/events')
-def events():
-    """
-    The different in-game events page
-    """
-    output = db.session.query(Event).all()
- 
-    return render_template('events.html', output = output)
-
-
-@flaskrouter.route('/events/<int:event_id>')
-def event(event_id):
-    data = models.Event.query.get(event_id)
-    return render_template('event_instance.html', data = data)
-
-
 @flaskrouter.route('/heroes')
 @flaskrouter.route('/heroes/page/<int:page>')
 def heroes(page=None):
@@ -50,7 +32,7 @@ def heroes(page=None):
       page = 1
     data = db.session.query(Hero).all()
     per_page = 6
-    output = data[per_page*(page-1):per_page*page]
+    output = data[per_page*(page-1) : per_page*page]
     pagination = Hero.query.paginate(per_page=per_page, page=page)
     return render_template('heroes.html', output=output, pagination = pagination, page_num = page)
 
@@ -60,25 +42,37 @@ def hero(hero_id):
     return render_template('heroes_instance.html', data = data)
 
 @flaskrouter.route('/players')
-def players():
+@flaskrouter.route('/players/page/<int:page>')
+def players(page=None):
     """
     A page for top-rated players
     """
-    output = db.session.query(TopPlayer).all()
-    return render_template('players.html', output=output)
+    if page == None:
+      page = 1
+    per_page = 9
+    data = db.session.query(TopPlayer).all()
+    output = data[per_page*(page-1) : per_page*page]
+    pagination = TopPlayer.query.paginate(per_page=per_page, page=page)
+    return render_template('players.html', output=output,pagination=pagination, page_num=page)
 
 @flaskrouter.route('/players/<int:top_player_id>')
 def player(top_player_id):
     data = models.TopPlayer.query.get(top_player_id)
-    return render_template('player_instance.html', data = data)
+    return render_template('player_instance.html', data=data)
 
 @flaskrouter.route('/skins')
-def skins():
+@flaskrouter.route('/skins/page/<int:page>')
+def skins(page=None):
     """
     A page for in-game skins
     """
-    output = db.session.query(Skin).all()
-    return render_template('skins.html', output=output)
+    if page == None:
+      page = 1
+    per_page = 9
+    data = db.session.query(Skin).all()
+    output = data[per_page*(page-1) : per_page*page]
+    pagination = Skin.query.paginate(per_page=per_page, page=page)
+    return render_template('skins.html', output=output, pagination=pagination, page_num=page)
 
 @flaskrouter.route('/skins/<int:skin_id>')
 def skin(skin_id):
@@ -86,12 +80,18 @@ def skin(skin_id):
     return render_template('skin_instance.html', data = data)
 
 @flaskrouter.route('/items')
-def items():
+@flaskrouter.route('/items/page/<int:page>')
+def items(page=None):
     """
     The page for in-game items 
     """
-    output = db.session.query(Item).all()
-    return render_template('items.html', output=output)
+    if page == None:
+      page = 1
+    per_page = 15
+    data = db.session.query(Item).filter(Item.hero_id != None)
+    output = data[per_page*(page-1) : per_page*page]
+    pagination = Item.query.filter(Item.hero_id != None).paginate(per_page=per_page, page=page)
+    return render_template('items.html', output=output, pagination=pagination, page_num=page)
 @flaskrouter.route('/items/<int:item_id>')
 def item(item_id):
     data = models.Item.query.get(item_id)
@@ -100,18 +100,47 @@ def item(item_id):
 
 
 @flaskrouter.route('/achievements')
-def achievements():
+@flaskrouter.route('/achievements/page/<int:page>')
+def achievements(page=None):
     """
     The different types of achievements page
     """
-    
-    output = db.session.query(Achievement).all()
-    return render_template('achievements.html', output=output)
+    if page == None:
+      page = 1
+    per_page = 12
+    data = db.session.query(Achievement).all()
+    output = data[per_page*(page-1) : per_page*page]
+    pagination = Achievement.query.paginate(per_page=per_page, page=page)
+    return render_template('achievements.html', output=output, pagination=pagination, page_num=page)
 
 
 @flaskrouter.route('/achievements/<int:achievement_id>')
 def achievement(achievement_id):
     data = models.Achievement.query.get(achievement_id)
     return render_template('achievement_instance.html',data = data)
+
+
+
+@flaskrouter.route('/events')
+@flaskrouter.route('/events/page/<int:page>')
+def events(page=None):
+    """
+    The different in-game events page
+    """
+    if page == None:
+      page = 1
+    per_page = 9
+    data = db.session.query(Event).all()
+    output = data[per_page*(page-1) : per_page*page]
+    pagination = Event.query.paginate(per_page=per_page, page=page)
+    return render_template('events.html', output=output, pagination=pagination, page_num=page)
+
+
+@flaskrouter.route('/events/<int:event_id>')
+def event(event_id):
+    data = models.Event.query.get(event_id)
+    return render_template('event_instance.html', data = data)
+
+
 
 
