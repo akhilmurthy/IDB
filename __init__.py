aae9d5ca3a14345194343563eed0d266ac70f755
hubaquired <hubaquired@users.noreply.github.com>
2017-11-28 15:04:04 -0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
 from flask import Flask 
 from flaskrouter import flaskrouter
+from flask_cors import CORS, cross_origin
 
 import models as models
 
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres+psycopg2://postgres:groupPassword@/postgres?host=/cloudsql/overwatchglam-181020:us-central1:overwatch2'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 models.db.init_app(app)
